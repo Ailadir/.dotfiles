ae53482eff9519bc2ba32489f0b0a4a4d29210bb
Араз Шварц <a.shvarts@team.superjob.ru>
2024-03-10 03:06:27 +0300

    returned to lazyvim(testing phase)

--- nvim/plugins/conform.lua
+++ nvim/plugins/conform.lua
@@ -1,52 +1,77 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufReadPost", "BufNewFile" },
+	event = "LspAttach",
 	opts = {
-		format = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-			async = false,
-		},
-
+		quiet = true,
 		formatters_by_ft = {
-			javascript = { { "eslint_d", "prettierd" } },
-			typescript = { { "eslint_d", "prettierd" } },
-			typescriptreact = { { "eslint_d", "prettierd" } },
-			javascriptreact = { { "eslint_d", "prettierd" } },
-			json = { "prettierd" },
-			-- yaml = { "prettierd" },
-			markdown = { "prettierd" },
-			-- graphql = { "prettierd" },
-			scss = { { "stylelint", "prettierd" } },
-			css = { { "stylelint", "prettierd" } },
-			html = { "prettierd" },
-			lua = { "stylua" },
-			-- fish = { "fish_indent" },
-			-- sh = { "shfmt" },
-			-- zsh = { "shfmt" },
-			-- python = { "isort", "black" },
-			["*"] = { "codespell" },
-			["_"] = { "trim_whitespace" },
+			typescript = { { "eslint_d", "prettier" } },
+			typescriptreact = { "prettier" },
+			javascript = { "prettier" },
+			javascriptreact = { "prettier" },
+			json = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			markdown = { "prettier" },
+			yaml = { "prettier" },
+			sh = { "beautysh" },
+			zsh = { "beautysh" },
 		},
-		formatters = {
-			prettierd = {
-				command = "prettierd",
-				args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-				rootPatterns = {
-					".prettierrc",
-					".prettierrc.json",
-					".prettierrc.toml",
-					".prettierrc.js",
-					".prettierrc.cjs",
-					".prettierrc.json5",
-					".prettierrc.yaml",
-					".prettierrc.yml",
-					"prettier.config.js",
-					"prettier.config.cjs",
-					"prettier.config.mjs",
-					".editorconfig",
-				},
-			},
-		},
+		-- format_on_save = function(bufnr)
+		-- 	-- Disable autoformat for files in a certain path
+		-- 	local bufname = vim.api.nvim_buf_get_name(bufnr)
+		-- 	if bufname:match("/node_modules/") then
+		-- 		return
+		-- 	end
+		--
+		-- 	return { timeout_ms = 500, lsp_fallback = true, async = true }
+		-- end,
+		-- format_after_save = { lsp_fallback = true },
 	},
+	-- config = function(_, opts)
+	-- 	local conform = require("conform")
+	-- 	conform.setup(opts)
+	--
+	-- 	-- Customize prettier args
+	-- 	require("conform.formatters.prettier").args = function(self, ctx)
+	-- 		local prettier_roots = { ".prettierrc", ".prettierrc.json", "prettier.config.js" }
+	-- 		local args = { "--stdin-filepath", "$FILENAME" }
+	--
+	-- 		local localPrettierConfig = vim.fs.find(prettier_roots, {
+	-- 			upward = true,
+	-- 			path = ctx.dirname,
+	-- 			type = "file",
+	-- 		})[1]
+	-- 		local globalPrettierConfig = vim.fs.find(prettier_roots, {
+	-- 			path = vim.fn.stdpath("config"),
+	-- 			type = "file",
+	-- 		})[1]
+	-- 		local disableGlobalPrettierConfig = os.getenv("DISABLE_GLOBAL_PRETTIER_CONFIG")
+	--
+	-- 		-- Project config takes precedence over global config
+	-- 		if localPrettierConfig then
+	-- 			vim.list_extend(args, { "--config", localPrettierConfig })
+	-- 		elseif globalPrettierConfig and not disableGlobalPrettierConfig then
+	-- 			vim.list_extend(args, { "--config", globalPrettierConfig })
+	-- 		end
+	--
+	-- 		local hasTailwindPrettierPlugin = vim.fs.find("node_modules/prettier-plugin-tailwindcss", {
+	-- 			upward = true,
+	-- 			path = ctx.dirname,
+	-- 			type = "directory",
+	-- 		})[1]
+	--
+	-- 		if hasTailwindPrettierPlugin then
+	-- 			vim.list_extend(args, { "--plugin", "prettier-plugin-tailwindcss" })
+	-- 		end
+	--
+	-- 		return args
+	-- 	end
+	--
+	-- 	conform.formatters.beautysh = {
+	-- 		prepend_args = function()
+	-- 			return { "--indent-size", "2", "--force-function-style", "fnpar" }
+	-- 		end,
+	-- 	}
+	-- end,
 }
