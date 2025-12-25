86ec07e4c08dd380c48af38296661a404dd3cbc9
Araz Schwarz <117507118+Ailadir@users.noreply.github.com>
2025-12-25 15:27:13 +0300

    Update of config

---
+++ nvim/lua/custom/plugins/mini-comment.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			require("mini.comment").setup({
+				options = {
+					custom_commentstring = function()
+						return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+					end,
+				},
+			})
+		end,
+	},
+}
